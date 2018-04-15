f0315fbd405b77c86a849cd2c017d514c95afc21
Dmitry Golubets <dgolubets@gmail.com>
2018-04-15 13:49:58 +0100

    Increase default timeout

--- src/main/scala/ru/dgolubets/reactjs/server/RenderServerSettings.scala
+++ src/main/scala/ru/dgolubets/reactjs/server/RenderServerSettings.scala
@@ -3,8 +3,9 @@ package ru.dgolubets.reactjs.server
 import java.io.File
 
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 case class RenderServerSettings(sources: Seq[ScriptSource],
                                 watch: Option[File] = None,
                                 nInstances: Int = Runtime.getRuntime.availableProcessors,
-                                requestTimeout: FiniteDuration = 30 seconds)
+                                requestTimeout: FiniteDuration = 120 seconds)
