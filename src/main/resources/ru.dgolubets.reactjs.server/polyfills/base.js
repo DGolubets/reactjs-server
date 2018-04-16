16ee738031da00d4f45d645dd21e509b96127faa
Dmitry Golubets <dgolubets@gmail.com>
2018-04-16 19:42:04 +0100

    Fix console polyfill for Graal

--- src/main/resources/ru.dgolubets.reactjs.server/polyfills/base.js
+++ src/main/resources/ru.dgolubets.reactjs.server/polyfills/base.js
@@ -1,43 +1,36 @@
-
-
-(function(context) {
+(function (context) {
     context.global = context;
 
     var logger = context.logger || Interop.import('logger');
 
-    if(!context.console){
-        context.console = {
-            log: function(){
-                for(var i = 0; i < arguments.length; i ++){
-                    logger.info(arguments[i])
-                }
-            },
-            info: function(){
-                for(var i = 0; i < arguments.length; i ++){
-                    logger.info(arguments[i])
-                }
-            },
-            debug: function(){
-                for(var i = 0; i < arguments.length; i ++){
-                    logger.debug(arguments[i])
-                }
-            },
-            trace: function(){
-                for(var i = 0; i < arguments.length; i ++){
-                    logger.debug(arguments[i])
-                }
-            },
-            warn: function(){
-                for(var i = 0; i < arguments.length; i ++){
-                    logger.warning(arguments[i])
-                }
-            },
-            error: function(){
-                for(var i = 0; i < arguments.length; i ++){
-                    logger.error(arguments[i])
-                }
-            }
-        };
-    }
+    context.console = context.console || {};
+
+    context.console.debug = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            logger.debug(arguments[i])
+        }
+    };
+
+    context.console.info = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            logger.info(arguments[i])
+        }
+    };
+
+    context.console.warn = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            logger.warning(arguments[i])
+        }
+    };
+
+    context.console.error = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            logger.error(arguments[i])
+        }
+    };
+
+    context.console.log = context.console.info;
+
+    context.console.trace = context.console.debug;
 
 })(this);
